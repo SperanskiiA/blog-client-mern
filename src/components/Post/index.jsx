4f851023c3440d319fc1ab979efb59ada8e35a0e
Speranskii-a <speranskii_a@mail.ru>
2023-02-13 13:12:52 +0300

    changed post component ui

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import clsx from 'clsx';
-import IconButton from '@mui/material/IconButton';
+
 import DeleteIcon from '@mui/icons-material/Clear';
 import EditIcon from '@mui/icons-material/Edit';
 import EyeIcon from '@mui/icons-material/RemoveRedEyeOutlined';
 import CommentIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import axios from '../../axios';
 import styles from './Post.module.scss';
 import { UserInfo } from '../UserInfo';
 import { PostSkeleton } from './Skeleton';
 import { fetchRemovalEl } from '../../redux/slices/post';
-import { Typography } from '@mui/material';
+
+import {
+  Card,
+  Typography,
+  IconButton,
+  CardMedia,
+  CardContent,
+  CardActionArea,
+  StyledEngineProvider,
+  CardActions,
+  Collapse,
+} from '@mui/material';
 import { CommentsBlock } from '../CommentsBlock';
 import { Comment } from '../AddComment';
 
@@ -35,6 +46,7 @@ export const Post = React.memo(
     const [visible, setVisible] = React.useState(false);
     const [shouldUpdate, setUpdate] = React.useState(false);
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     const update = shouldUpdate === true;
     const date = createdAt?.split('').slice(0, 10).join('');
@@ -54,6 +66,7 @@ export const Post = React.memo(
           .then((res) => {
             setUpdate(false);
             setComs(res.data);
+            res.data && console.log('fetch comments' + res.data);
           })
           .catch((err) => console.log('err is:  ' + err));
       _id && isFullPost && setVisible(true);
@@ -69,77 +82,107 @@ export const Post = React.memo(
 
     return (
       <>
-        <div className={clsx(styles.root, { [styles.rootFull]: isFullPost })}>
-          {isEditable && (
-            <div className={styles.editButtons}>
-              <Link to={`/posts/${_id}/edit`}>
-                <IconButton color="primary">
-                  <EditIcon />
-                </IconButton>
-              </Link>
-              <IconButton onClick={onClickRemove} color="secondary">
-                <DeleteIcon />
-              </IconButton>
-            </div>
-          )}
-          {imageUrl && (
-            <img
-              className={clsx(styles.image, { [styles.imageFull]: isFullPost })}
-              src={imageUrl}
-              alt={title}
-            />
-          )}
-          <div className={styles.wrapper}>
-            <UserInfo {...user} additionalText={date} />
-            <div className={styles.indention}>
-              <h2
-                className={clsx(styles.title, {
-                  [styles.titleFull]: isFullPost,
-                })}
-              >
-                {isFullPost ? title : <Link to={`/posts/${_id}`}>{title}</Link>}
-              </h2>
-              <ul className={styles.tags}>
-                {tags?.map((name) => (
-                  <li key={name}>
-                    <Link to={`/tags/${name}`}>#{name}</Link>
-                  </li>
-                ))}
-              </ul>
-              {children && <div className={styles.content}>{children}</div>}
-              <ul className={styles.postDetails}>
-                <li>
-                  <EyeIcon htmlColor="#000" />
-                  <span>{viewsCount}</span>
-                </li>
-                <li>
-                  <IconButton onClick={showCommentsHandler}>
-                    <CommentIcon htmlColor={!visible ? '#000' : '#8007e3'} />
-                    <Typography
-                      sx={{
-                        fontSize: '14px',
-                        color: !visible ? '#000' : ' #8007e3',
-                      }}
-                    >
-                      {commentsCount ? commentsCount : coms?.length}
-                    </Typography>
+        <StyledEngineProvider injectFirst>
+          <Card
+            sx={{ margin: '20px 0' }}
+            className={clsx(styles.root, { [styles.rootFull]: isFullPost })}
+          >
+            <CardActionArea
+              onClick={
+                !isFullPost
+                  ? () => navigate(`/posts/${_id}`)
+                  : showCommentsHandler
+              }
+            >
+              {imageUrl && (
+                <CardMedia
+                  image={imageUrl ? imageUrl : ''}
+                  sx={{ width: '100%', height: 'auto', aspectRatio: '4/2' }}
+                />
+              )}
+              <CardContent>
+                <UserInfo {...user} additionalText={date} />
+                <div className={styles.indention}>
+                  <Typography
+                    variant="h3"
+                    className={clsx(styles.title, {
+                      [styles.titleFull]: isFullPost,
+                    })}
+                  >
+                    {title}
+                  </Typography>
+                  <ul className={styles.tags}>
+                    {tags?.map((name) => (
+                      <li key={name}>
+                        <Link to={`/tags/${name}`}>#{name}</Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+                {children && <div className={styles.content}>{children}</div>}
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              {isEditable && (
+                <div className={styles.editButtons}>
+                  <Link to={`/posts/${_id}/edit`}>
+                    <IconButton color="primary">
+                      <EditIcon />
+                    </IconButton>
+                  </Link>
+                  <IconButton onClick={onClickRemove} color="secondary">
+                    <DeleteIcon />
                   </IconButton>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        {visible &&
-          coms &&
-          (coms.length > 0 ? (
-            <CommentsBlock items={coms} setUpdate={handleUpdate}>
-              <Comment post_id={_id} user={user} setUpdate={handleUpdate} />
-            </CommentsBlock>
-          ) : (
-            <>
-              <Comment post_id={_id} user={user} setUpdate={handleUpdate} />
-            </>
-          ))}
+                </div>
+              )}
+              <div className={styles.indention}>
+                <ul className={styles.postDetails}>
+                  <li>
+                    <EyeIcon htmlColor="#000" />
+                    <span>{viewsCount}</span>
+                  </li>
+                  <li>
+                    <IconButton
+                      onClick={showCommentsHandler}
+                      aria-expanded={visible}
+                    >
+                      <CommentIcon htmlColor={!visible ? '#000' : '#4361ee'} />
+                      <Typography
+                        sx={{
+                          fontSize: '14px',
+                          color: !visible ? '#000' : ' #4361ee',
+                        }}
+                      >
+                        {commentsCount ? commentsCount : coms?.length}
+                      </Typography>
+                    </IconButton>
+                  </li>
+                </ul>
+              </div>
+            </CardActions>
+            <Collapse in={visible} timeout="auto" unmountOnExit>
+              <div className={styles.collapse}>
+                {coms.length > 0 ? (
+                  <CommentsBlock items={coms} setUpdate={handleUpdate}>
+                    <Comment
+                      post_id={_id}
+                      user={user}
+                      setUpdate={handleUpdate}
+                    />
+                  </CommentsBlock>
+                ) : (
+                  <>
+                    <Comment
+                      post_id={_id}
+                      user={user}
+                      setUpdate={handleUpdate}
+                    />
+                  </>
+                )}
+              </div>
+            </Collapse>
+          </Card>
+        </StyledEngineProvider>
       </>
     );
   }
